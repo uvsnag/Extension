d61808c77b4a66eb6e5edd524e4d8b708037d805
nhu phuong <sangnv046@gmail.com>
2022-10-11 16:52:16 +0700

    -

--- logListentPra/background.js
+++ logListentPra/background.js
@@ -1,24 +1,32 @@
 var inteSubYt = null;
 var inteOpenSub = null;
 var inteGetSub = null;
-const ID ='id-log-ssnag'
-const url = 'https://uvsnag.github.io/voiceToText'
+const ID = 'id-log-ssnag'
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
   if (msg.command == 'create-ele') {
     exstElement = document.getElementById(ID)
-    if(exstElement){
+    if (exstElement) {
       exstElement.remove()
     }
+    console.log('data reci..:')
+      console.log(msg.data)
     const para = document.createElement("p");
-    para.setAttribute("id",ID);
-    const node = document.createTextNode("This is a paragraph."+msg.data.msg);
-
+    para.setAttribute("id", ID);
+    const node = document.createTextNode(msg.data);
     para.appendChild(node);
-    document.querySelector(".input_and_continue.row").appendChild(para);;
-    chrome.runtime.sendMessage({ command: "getlog-complete", data: 'OK' });
-  }else if(msg.command == 'get-log'){
-    const text = document.querySelector('.content-log')
+    document.querySelector(msg.selector).appendChild(para);;
+  } else if (msg.command == 'get-log') {
+    var ele = document.querySelector('#transcript-i')
+    let text = ele.value
+    if (!text) {
+      text = ele.innerHTML
+      if (!text) {
+        text = ele.textContent
+      }
+    }
+    console.log('text:')
+    console.log(text)
     chrome.runtime.sendMessage({ command: "getlog-complete", data: text });
   }
 });
--- logListentPra/popup.js
+++ logListentPra/popup.js
@@ -1,46 +1,32 @@
-const URL_YT = "www.youtube.com/watch?";
-const URL_GITHUB_YTSUB = "https://uvsnag.github.io/voiceToText";
+// const URL_DENSTINY_SITE = "https://uvsnag.github.io/voiceToText";
+const URL_DENSTINY_SITE = "http://localhost:3000/voiceToText";
 
 var intv = null
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
   if (msg.command == 'getlog-complete') {
-
-    text
-
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      var type = 'get-log';
       var activeTab = tabs[0];
-      console.log('send')
-      var msg = 1
-      // intv = setInterval(() => {
-  
-        let sytTab = getTabYtSub(tabs, URL_GITHUB_YTSUB);
-        chrome.tabs.executeScript({
-          code: '(' + modifyDOM + ')();' 
-      }, (results) => {
-          console.log('Popup script:')
-          console.log(results[0]);
-      });
-  
-        chrome.tabs.sendMessage(activeTab.id, { command: "create-ele", data: {type: type, msg:msg} });
-        msg++
-      // }, 1000);
+      var selector = document.getElementById('selector').value
+      console.log('selector:')
+      console.log(selector)
+      chrome.tabs.sendMessage(activeTab.id, { command: "create-ele", data: msg.data, selector:selector });
     });
   }
 });
 
 
 
-document.querySelector('.get-log').addEventListener('click', function () {
-
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    let sytTab = getTabYtSub(tabs, URL_GITHUB_YTSUB);
-    chrome.tabs.sendMessage(sytTab, { command: "get-log", data: {type: type, msg:msg} });
+document.querySelector('#get-log').addEventListener('click', function () {
+  chrome.tabs.query({  }, function (tabs) {
+    let sytTab = getTabYtSub(tabs, URL_DENSTINY_SITE);
+    intv = setInterval(() => {
+      console.log('sending---')
+      chrome.tabs.sendMessage(sytTab.id, { command: "get-log" });
+    }, 200);
   });
-
-  document.querySelector('.stop').addEventListener('click', function () {
-    clearInterval(intv)
-  })
+});
+document.querySelector('#stop').addEventListener('click', function () {
+  clearInterval(intv)
 });
 
 function getTabYtSub(tabs, url) {
